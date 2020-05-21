9dda972a8721a411a108018c794fd8b269563f4d
Kaneko Kenya <kaneko@atsu-maru.co.jp>
2020-05-21 21:28:30 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>テスト</h1>
+    <ChildrenComponent
+      v-model="foo"
+    >
+    </ChildrenComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChildrenComponent from './components/ChildrenComponent.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ChildrenComponent
+  },
+
+  data () {
+    return {
+      foo: []
+    }
   }
 }
 </script>
---
+++ src/components/ChildrenComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <label><input type="checkbox" @change="toggle('april')" value="april">4月</label>
+    <label><input type="checkbox" @change="toggle('july')" value="july">5月</label>
+    <label><input type="checkbox" @change="toggle('june')" value="june">6月</label>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'foo',
+  },
+
+  props: {
+    foo: Array
+  },
+
+  mounted () {
+    console.log(this.foo)
+  },
+
+  methods: {
+    toggle(value) {
+      let newIds = this.foo
+      // id が既にあるなら削除、無いなら追加
+      if (this.foo.includes(value)) {
+        newIds = newIds.filter(_id => _id != value)
+      } else {
+        newIds.push(value)
+      }
+
+      let uniqNewIds = Array.from(new Set(newIds))
+
+      console.log(uniqNewIds)
+
+      this.$emit('input', uniqNewIds)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
